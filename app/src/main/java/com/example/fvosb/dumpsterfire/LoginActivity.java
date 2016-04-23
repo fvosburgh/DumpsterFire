17ca4f749dd1dfc946a0475ba72d6773e0733fa5
Forester Vosburgh <fvosburgh3@gmail.com>
2016-04-23 17:29:37 -0400

    Add login page and functionality skeleton page

--- app/src/main/java/com/example/fvosb/dumpsterfire/LoginActivity.java
+++ app/src/main/java/com/example/fvosb/dumpsterfire/LoginActivity.java
@@ -1,7 +1,11 @@
 package com.example.fvosb.dumpsterfire;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login(v);
+            }
+        });
+    }
+
+    public void login(View view) {
+        EditText usernameText = (EditText) findViewById(R.id.LoginUsername);
+        EditText passwordText = (EditText) findViewById(R.id.LoginPassword);
+
+        String username = usernameText.getText().toString();
+        String password = passwordText.getText().toString();
+
+        if(username.equals("test") && password.equals("password")) {
+            startActivity(new Intent(this, FunctionalityActivity.class));
+        }
     }
 }
