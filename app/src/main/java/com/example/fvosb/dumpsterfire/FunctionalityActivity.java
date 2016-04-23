5d23e488030dbe9b3acc2a9ba0652346066bac97
Forester Vosburgh <fvosburgh3@gmail.com>
2016-04-23 17:53:55 -0400

    populate functionality page and get logout button working

--- app/src/main/java/com/example/fvosb/dumpsterfire/FunctionalityActivity.java
+++ app/src/main/java/com/example/fvosb/dumpsterfire/FunctionalityActivity.java
@@ -1,7 +1,10 @@
 package com.example.fvosb.dumpsterfire;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FunctionalityActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class FunctionalityActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_functionality);
+
+        Button viewSchedule = (Button) findViewById(R.id.viewScheduleButton);
+        Button makeReservation = (Button) findViewById(R.id.makeReservationButton);
+        Button updateReservation = (Button) findViewById(R.id.updateReservationButton);
+        Button cancelReservation = (Button) findViewById(R.id.cancelReservationButton);
+        Button giveReview = (Button) findViewById(R.id.giveReviewButton);
+        Button addSchoolInfo = (Button) findViewById(R.id.addSchoolInfoButton);
+        Button logout = (Button) findViewById(R.id.logoutButton);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout(v);
+            }
+        });
+    }
+
+    public void logout(View v) {
+        // member manager maybe
+        startActivity(new Intent(this, LoginActivity.class));
     }
 }
